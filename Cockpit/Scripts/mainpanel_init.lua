6d72bed3892ca1f35a36ffe796a1f95942cd480c
jjjjj103074 <96659427+jjjjj103074@users.noreply.github.com>
2026-01-30 12:11:56 +0800

    基礎架構完成(成功進入駕駛模式)

---
+++ Cockpit/Scripts/mainpanel_init.lua
@@ -0,0 +1,11 @@
+-- -- 定義這是一個儀表板
+-- shape_name = "default"
+-- livery = "default"
+
+-- is_3d = true 
+
+-- -- 對應到 device_init.lua 的定義
+-- local controllers = LoRegisterPanelControls()
+
+-- -- 這裡必須留空，但必須存在，讓 DCS 知道儀表板邏輯已就緒
+-- need_to_be_closed = true
